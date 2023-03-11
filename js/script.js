263560ab023e7a3e25aa4bba54c4bd5fc5770955
DevIItalo <italoppttuu@gmail.com>
2023-03-11 13:18:34 -0300

    Commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const play = document.querySelector("#play");
+const pause = document.querySelector("#pause");
+
+play.addEventListener('click', ()=> {
+    pause.style.display = 'block'
+
+});
+
+function tocarMusica(){
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
+}
+
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
+}
